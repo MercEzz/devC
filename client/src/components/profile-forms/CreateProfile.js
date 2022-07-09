69600942dfab9d49fc4dac99f0211405c1745c29
mitesh virash <miteshkvirash@gmail.com>
2022-07-09 17:17:15 +0530

    create & edit profile pg updated

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -208,7 +208,7 @@ const CreateProfile = ({ createProfile }) => {
           </>
         )}
         <input type="submit" className="btn btn-primary my-1" />
-        <Link className="btn btn-light my-1" to="dashboard">
+        <Link className="btn btn-light my-1" to="/dashboard">
           Go Back
         </Link>
       </form>
