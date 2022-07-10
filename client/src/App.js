7836faae9a91da357fefcfd4cfc235bd54f4ae32
mitesh virash <miteshkvirash@gmail.com>
2022-07-10 19:42:31 +0530

    post action and reducer added

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -59,6 +60,7 @@ function App() {
             path="/add-education"
             element={<PrivateRoute component={AddEducation} />}
           />
+          <Route path="/posts" element={<PrivateRoute component={Posts} />} />
         </Routes>
       </BrowserRouter>
     </Provider>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -65,7 +65,14 @@ export const getProfileById = (userId) => async (dispatch) => {
 // Get Github repos
 export const getGithubRepos = (username) => async (dispatch) => {
   try {
-    const res = await axios.get(`/api/profile/github/${username}`);
+    const api = axios.create({
+      baseURL: "/api",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const res = await api.get(`/profile/github/${username}`);
     dispatch({
       type: GET_REPOS,
       payload: res.data,
