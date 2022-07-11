6a1edba5a86b45b7951a02637754bd0fea027e1f
mitesh virash <miteshkvirash@gmail.com>
2022-07-11 09:18:21 +0530

    post form added

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+import { ADD_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -56,11 +56,36 @@ export const removeLike = (id) => async (dispatch) => {
 // Delete post
 export const deletePost = (id) => async (dispatch) => {
   try {
-    const res = await axios.delete(`/api/posts/${id}`);
+    await axios.delete(`/api/posts/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
       payload: id,
+    });
+
+    dispatch(setAlert("Post Created", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add post
+export const addPost = (formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const res = await axios.post(`/api/posts`, formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
     });
 
     dispatch(setAlert("Post deleted", "success"));
